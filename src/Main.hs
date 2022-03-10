e1397ff839af35286f09cd52ff81079a0975e756
dgpratt <1676341+dgpratt@users.noreply.github.com>
2022-03-10 13:37:28 -0500

    Apply minor formatting.

--- src/Main.hs
+++ src/Main.hs
@@ -9,4 +9,4 @@ main = getArgs >>= run
 run :: [String] -> IO ()
 run ("-v":"atan":_) = putStrLn (piDigitsAtan 1000)
 run ("-v":"asin":_) = putStrLn (piDigitsAsin 1000)
-run _            = putStrLn (piDigits 1000)
+run _               = putStrLn (piDigits 1000)
